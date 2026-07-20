1208bcb17ba754088b12b3e51656c07fb3a7d15e
sahasourav17 <sahasourav1170@gmail.com>
2026-07-20 11:52:31 +0600

    setup environment and db connection

--- src/main/java/com/backend/banking_api/BankingApiApplication.java
+++ src/main/java/com/backend/banking_api/BankingApiApplication.java
@@ -1,13 +1,22 @@
 package com.backend.banking_api;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankingApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApiApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Spring Boot application started");
+		};
 	}
 
 }
